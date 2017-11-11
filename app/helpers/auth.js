d1cd3b8b8bb83c0aa4d3e1c409da40cc423d6984
Jose Saint Hilaire <itt.jose.sainthilaire@gmail.com>
2017-11-11 19:33:59 -0400

    Add JSDoc to the auth helper

--- app/helpers/auth.js
+++ app/helpers/auth.js
@@ -2,14 +2,27 @@ import { auth, facebookAuthProvider, googleAuthProvider } from 'config/firebase'
 
 const signedInStatus = 'SIGNED_IN'
 
+/**
+ * Login the user with firebase
+ * @param {string} provider - provider to login against
+ * @returns {firebase.Promise.<firebase.auth.UserCredential>|Promise<any>}
+ */
 export const authenticate = (provider = 'google') => {
   const providerInstance = provider !== 'google' ? facebookAuthProvider : googleAuthProvider
 
   return auth.signInWithPopup(providerInstance)
 }
 
+/**
+ * Checks if the user is logged in
+ * @param {string} status - represents the current user status
+ * @param {string} uid - id of the current user
+ */
 export const isAuthenticated = (status = '', uid) => status === signedInStatus && !!uid
 
+/**
+ * Log the user out of the platform
+ */
 export const logout = () => {
   auth.signOut()
 }
