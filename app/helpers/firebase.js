d540f58b90391cb4741c5e7e820a61f2469b6584
Jose Saint Hilaire <itt.jose.sainthilaire@gmail.com>
2017-11-11 19:48:38 -0400

    Add JSDoc to the firebase helpers

--- app/helpers/firebase.js
+++ app/helpers/firebase.js
@@ -1,6 +1,11 @@
 import { db, storage } from 'config/firebase'
 import { addExtendedDataToPlace, removeUpdateMetadata } from './format'
 
+/**
+ * Saves user into the firebase database
+ * @param {Object} user - user data object
+ * @returns {Promise.<TResult>}
+ */
 export const saveUser = (user) => {
   const {
     uid, displayName, photoURL, email,
@@ -15,6 +20,13 @@ export const saveUser = (user) => {
     .then(() => user)
 }
 
+/**
+ *
+ * @param {string} uid - id of the user who wants to save the image
+ * @param {string} placeId - id of the image attached place
+ * @param {Object} image - metadata of the image
+ * @returns {Promise}
+ */
 const saveImage = (uid, placeId, image) => {
   const storageRef = storage.ref('/places').child(`${uid}/${placeId}`)
   const upload = storageRef.child(image.name)
@@ -27,6 +39,12 @@ const saveImage = (uid, placeId, image) => {
   return upload
 }
 
+/**
+ *
+ * @param {string} uid - id of the user who wants to save the place
+ * @param {Object} place - data of the place to be added
+ * @returns {Promise.<TResult>}
+ */
 export const savePlace = (uid, place) => {
   const { image } = place
   const placeRef = db.collection('places').doc()
@@ -45,6 +63,11 @@ export const savePlace = (uid, place) => {
   })
 }
 
+/**
+ * Formats multiple objects coming from firebase
+ * @param {Object} snapshot - object comming from a firebase fetch
+ * @returns {{}}
+ */
 export const snapshotToObject = (snapshot) => {
   const returnObj = {}
 
@@ -55,17 +78,31 @@ export const snapshotToObject = (snapshot) => {
   return returnObj
 }
 
+/**
+ * Fetch places data from firebase
+ * returns {undefined} undefined
+ */
 export const getPlaces = () => db.collection('places')
   .get()
   .then(querySnapshot => snapshotToObject(querySnapshot))
 
 
+/**
+ * Get saved places for an specific user
+ * @param {string} uid - id of the users who the places belongs
+ * @returns {undefined} undefined
+ */
 export const getUserPlaces = uid => db.collection('userPlaces')
   .where('uid', '==', uid)
   .get()
   .then(querySnapshot => snapshotToObject(querySnapshot))
 
 
+/**
+ * Get specific user by user id
+ * @param {string} uid - id of the user to be fetched
+ * @returns {Object}
+ */
 export const getUser = uid => db.collection('users').doc(uid).get().then((doc) => {
   if (doc.exists) {
     return doc.data()
@@ -75,7 +112,12 @@ export const getUser = uid => db.collection('users').doc(uid).get().then((doc) =
   return {}
 })
 
-
+/**
+ * Save a new place for an specific user
+ * @param {Object} place - data of the place
+ * @param {string} uid - id of the user who is saving the place
+ * @returns {Promise.<TResult>}
+ */
 export const saveUserPlaceToVisit = (place, uid) => {
   const { imageURL } = place
 
