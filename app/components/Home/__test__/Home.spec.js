f9eb501f57616852cc22f5ff84826ec3661eac93
Jose Saint Hilaire <itt.jose.sainthilaire@gmail.com>
2017-11-11 17:16:24 -0400

    Add Home test

---
+++ app/components/Home/__test__/Home.spec.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { shallow } from 'enzyme'
+import toJson from 'enzyme-to-json'
+import Home from '../Home'
+
+test('Home should render correctly', () => {
+  const wrapper = shallow(<Home />)
+  expect(toJson(wrapper)).toMatchSnapshot()
+})
