7096f425fe7cf0953563b9a15d523fb20012b523
Jose Saint Hilaire <itt.jose.sainthilaire@gmail.com>
2017-11-11 19:53:12 -0400

    Add JSDoc to the format helpers

--- app/helpers/format.js
+++ app/helpers/format.js
@@ -1,6 +1,11 @@
 import omit from 'lodash/omit'
 import { serverTimestamp } from 'config/firebase'
 
+/**
+ * Adds metadata to the places
+ * @param {Object} place - data of the place
+ * @returns {Object}
+ */
 export const formatPlace = (place) => {
   const { name, description } = place
 
@@ -12,9 +17,20 @@ export const formatPlace = (place) => {
   }
 }
 
+/**
+ * Adds data which extends the place objects
+ * @param {Object} place - data of the place
+ * @param {Object} extendedData - data to be added to places
+ * @returns {Object}
+ */
 export const addExtendedDataToPlace = (place, extendedData) => ({
   ...formatPlace(place),
   ...extendedData,
 })
 
+/**
+ * Removes metadata from firebase objects
+ * @param {Object} data - object containing the data and metadata
+ * @returns {Object}
+ */
 export const removeUpdateMetadata = data => omit(data, ['createAt', 'updatedAt'])
